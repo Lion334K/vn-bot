277a72ce05f6c996b8c79c9d72dd58ca6cff24c0
Lion334K <swippintree334@gmail.com>
2026-08-12 23:40:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ from typing import Literal
 #  1. KANAL & ID AYARLARI
 # ───────────────────────────────────────────────
 
-TOKEN = "MTQ3ODg0MjI0NzY2NjU5Nzk4MA.GS36cj.8ICIz9I8YiyeN3qXBzZbD52KSBTm2t1iKYZRnI"
+TOKEN = "MTQ3ODg0MjI0NzY2NjU5Nzk4MA.GN2Wm_.3kQUTIH3ZVodJyNys_0NSqllciSeyMacTEHbf0"
 
 WELCOME_CHANNEL_ID         = 1488662459009994965
 BUMP_CHANNEL_ID            = 1381771230964748370
